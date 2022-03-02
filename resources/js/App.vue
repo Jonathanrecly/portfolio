b7d7048e334fa9cb7aafd31882b1ef0f95374693
Jonathan Recly <j.recly@gmail.com>
2022-03-02 10:36:36 +0400

    create first presentation

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,12 +1,59 @@
 <template>
-    <div>
-        dans vue
-    </div>
+    <v-app>
+        <v-main>
+            <v-container>
+                <v-card>
+                    <v-card-title>
+                        <span class="text-h3">
+                            Jonathan Recly : Portfolio
+                        </span>
+                    </v-card-title>
+                    <v-card-text>
+                        <v-container fluid>
+                            <v-row>
+                                <v-col
+                                    cols="12"
+                                    sm="3"
+                                    v-for="project in projects"
+                                    :key="project.id"
+                                >
+                                    <v-card
+                                        @click="goTo(project)"
+                                    >
+                                        <v-card-title>
+                                            {{ project.name }}
+                                            <v-spacer></v-spacer>
+                                            <v-icon>mdi-eye</v-icon>
+                                        </v-card-title>
+                                    </v-card>
+                                </v-col>
+                            </v-row>
+                        </v-container>
+                    </v-card-text>
+                </v-card>
+
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
 
 <script>
     export default {
-
+        data: () => ({
+            projects: [
+                {
+                    id: 1,
+                    name: "Todo list",
+                    description: "",
+                    target: ""
+                }
+            ]
+        }),
+        methods: {
+            goTo(project) {
+                console.log(project)
+            }
+        }
     }
 </script>
 
