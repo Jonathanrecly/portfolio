23453ff8b53d288390d7e7bd34df87d6cdf97350
Jonathan Recly <j.recly@gmail.com>
2022-03-04 08:37:02 +0400

    add dynamic routing

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,11 @@ use App\Http\Controllers;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::domain('todo.jrecly.local')->group(function () {
+Route::domain('todo.' . env("APP_HOST"))->group(function () {
     Route::get('/', Controllers\Todo\HomeController::class);
 });
 
-Route::domain('menu_circle.jrecly.local')->group(function () {
+Route::domain('menu_circle.' . env("APP_HOST"))->group(function () {
     Route::get('/', Controllers\MenuCircleHomeController::class);
 });
 
